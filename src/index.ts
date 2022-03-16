f22f9f8e3aac90c2d2cbdd1834c8bfec5aec8100
Akash Dathan <akashdathan@gmail.com>
2022-03-16 15:19:32 +0530

    feat: cli implemented

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,58 @@
 import * as readline from 'readline';
 import { Coincap } from './service/coincap';
+import { Exchange } from './service/exchange';
 
 export class TokenSale {
   private static async init() {
+    console.log('Fetching Latest Asset Prices...');
     await Coincap.init();
+    console.log('Asset Prices Fetched.');
+  }
+
+  private static validateInput(inputArray: string[]) {
+    if (inputArray.length !== 4) throw new Error('Invalid Input');
   }
 
   static async run() {
+    console.log('Initialising Service...');
     await this.init();
+    console.log('Service Initialisation Done.');
+
+    console.log('\n------------------------------------------------------------------------------------------');
+    console.log('Please Provide Input In The Following Format:');
+    console.log('<ETHSALE rate> <SALE decimal places> <Purchase currency> <BTC/ETH/DOGE purchase amount>');
+    console.log('------------------------------------------------------------------------------------------\n');
 
     const rl = readline.createInterface({
       input: process.stdin,
     });
 
-    rl.on('line', (line: string) => {
-      console.log(line);
-      const price = Coincap.getPriceInUsd('bitcoin');
-      console.log(price);
+    rl.on('line', async (line: string) => {
+      const inputArray = line.split(' ');
+      this.validateInput(inputArray);
+
+      const [
+        ethSaleRate,
+        outputDecimalRound,
+        purchaseCurrency,
+        purchaseAmount,
+      ] = inputArray;
+
+      const price = await Exchange.getExchangeRate(
+        ethSaleRate,
+        outputDecimalRound,
+        purchaseCurrency,
+        purchaseAmount,
+      );
+
+      console.log(JSON.stringify({
+        input: line,
+        ethSaleRate,
+        outputDecimalRound,
+        purchaseCurrency,
+        purchaseAmount,
+        outputPrice: price,
+      }, null, '\t'));
     });
   }
 }
